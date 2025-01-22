46605977430b7cba747d83f1a3ab7d92fada21da
OpenDEM <contact@opendemdata.info>
2025-01-22 21:02:57 +0100

    Update deleteTree.py

--- server/deleteTree.py
+++ server/deleteTree.py
@@ -2,15 +2,26 @@
 import cgi
 import json
 import psycopg2
+from dotenv import load_dotenv
+import os 
+
+load_dotenv('/home/python_env/config.env')
+
 try:
     arguments = cgi.FieldStorage()
     uuid = arguments.getvalue("id")
     comment = arguments.getvalue("comment")
-    conn = psycopg2.connect("host=yourHost dbname=yourDB user=yourUser password=yourPassword")
+    conn = psycopg2.connect(
+       user = os.getenv("DATABASE_USERNAME"),
+       password = os.getenv("DATABASE_PASSWORD"),
+       host = os.getenv("DATABASE_IP"),
+       port = os.getenv("DATABASE_PORT"),
+       database = os.getenv("DATABASE_NAME")
+    )
     cur = conn.cursor()
-    cur.execute("UPDATE trees SET comment_editor = %s WHERE tree_id = %s", [comment,uuid])
-    cur.execute("INSERT INTO trees_deleted SELECT * FROM trees WHERE tree_id = %s", [uuid])
-    cur.execute("DELETE FROM trees WHERE tree_id = %s", [uuid])
+    cur.execute("UPDATE trees_3857 SET comment_editor = %s WHERE tree_id = %s", [comment,uuid])
+    cur.execute("INSERT INTO trees_deleted_3857 SELECT gid, uuid, tree_id, name_ger, name_src, age_group, age_g_src, source, comment, watered, age, age_src, radius, radius_src, geom, watered_at, subsoil, comment_editor, create_edit FROM trees_test_3857 WHERE tree_id = %s", [uuid])
+    cur.execute("DELETE FROM trees_3857 WHERE tree_id = %s", [uuid])
     conn.commit()
     cur.close()
     conn.close()
