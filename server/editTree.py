365c039516a91d11ce49e5f78e34a075d946c67e
OpenDEM <contact@opendemdata.info>
2025-01-22 21:03:50 +0100

    Update editTree.py

--- server/editTree.py
+++ server/editTree.py
@@ -3,6 +3,11 @@ import cgi
 import json
 import psycopg2
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os 
+
+load_dotenv('/yourpath/.env')
+
 try:
     date_object = datetime.today()
     date_string = date_object.strftime("%Y-%m-%d")
@@ -34,9 +39,15 @@ try:
         age_g_src = None
     subsoil = arguments.getvalue("subsoil")
     comment = arguments.getvalue("comment")
-    conn = psycopg2.connect("host=yourHost dbname=yourDB user=yourUser password=yourPassword")
+    conn = psycopg2.connect(
+       user = os.getenv("DATABASE_USERNAME"),
+       password = os.getenv("DATABASE_PASSWORD"),
+       host = os.getenv("DATABASE_IP"),
+       port = os.getenv("DATABASE_PORT"),
+       database = os.getenv("DATABASE_NAME")
+    )
     cur = conn.cursor()
-    cur.execute("UPDATE trees SET name_ger = %s, name_src = %s, age = %s, age_src = %s, age_group = %s, age_g_src = %s, radius = %s, radius_src = %s,comment_editor = %s, subsoil= %s,create_edit = %s WHERE tree_id = %s", (name_ger,name_src,age,age_src,age_group,age_g_src,radius,radius_src,comment,subsoil,date_string,uuid))
+    cur.execute("UPDATE trees_3857 SET name_ger = %s, name_src = %s, age = %s, age_src = %s, age_group = %s, age_g_src = %s, radius = %s, radius_src = %s,comment_editor = %s, subsoil= %s,create_edit = %s WHERE tree_id = %s", (name_ger,name_src,age,age_src,age_group,age_g_src,radius,radius_src,comment,subsoil,date_string,uuid))
     conn.commit()
     cur.close()
     conn.close()
