f855c9a0102d31029d8ac458d36b2214aefee03e
OpenDEM <contact@opendemdata.info>
2025-01-22 21:02:27 +0100

    Update createTree.py

--- server/createTree.py
+++ server/createTree.py
@@ -4,12 +4,17 @@ import json
 import psycopg2
 import uuid
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os 
+
+load_dotenv('/yourpath/.env')
+
 try:
     date_object = datetime.today()
     date_string = date_object.strftime("%Y-%m-%d")
     date_giessen = datetime(year=2020, month=7, day=26, hour=4, minute=00, second=00)
     date_string_giessen = date_giessen.strftime("%Y-%m-%d")
-    uuid = str(uuid.uuid4())
+    tree_uuid = str(uuid.uuid4())
     arguments = cgi.FieldStorage()
     lat = arguments.getvalue("lat")
     lon = arguments.getvalue("lon")
@@ -28,9 +33,16 @@ try:
     source = 'crowd'
     age_group = arguments.getvalue("age_group")
     age_g_src = 'crowd'    
-    conn = psycopg2.connect("host=yourHost dbname=yourDB user=yourUser password=yourPassword")
+    conn = psycopg2.connect(
+       user = os.getenv("DATABASE_USERNAME"),
+       password = os.getenv("DATABASE_PASSWORD"),
+       host = os.getenv("DATABASE_IP"),
+       port = os.getenv("DATABASE_PORT"),
+       database = os.getenv("DATABASE_NAME")
+    )
     cur = conn.cursor()
-    cur.execute("INSERT INTO trees (geom,tree_id, uuid,name_ger,name_src,source,create_edit,age_group,age_g_src,radius,radius_src,age,age_src,subsoil,comment_editor,watered_at) VALUES (st_setsrid(st_makepoint(%s,%s),4326),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (lat,lon,uuid,uuid,name_ger,name_src,source,date_string,age_group,age_g_src,radius,radius_src,age,age_src,subsoil,comment,date_string_giessen))
+    #cur.execute("INSERT INTO trees_3857 (geom,tree_id, uuid,name_ger,name_src,source,create_edit,age_group,age_g_src,radius,radius_src,age,age_src,subsoil,comment_editor,watered_at) VALUES (ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326),3857),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (lat,lon,tree_uuid,tree_uuid,name_ger,name_src,source,date_string,age_group,age_g_src,radius,radius_src,age,age_src,subsoil,comment,date_string_giessen))    
+    cur.execute("INSERT INTO trees_3857 (geom,tree_id) VALUES (ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326),3857),%s)", (lat,lon,tree_uuid))    
     conn.commit()
     cur.close()
     conn.close()
